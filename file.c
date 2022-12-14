060c6e4acdd9299fdaaa561432be0f83c0bb89dd
mcsavvy <davemcsavvii@gmail.com>
2022-12-14 15:49:15 -0800

    Setting Up

---
+++ file.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+
+char *read_file(int fd)
+{
+	size_t file_size;
+	char *buffer;
+	struct stat filestat;
+	
+	fstat(fd, &filestat);
+	file_size = filestat.st_size;
+	buffer = malloc(file_size + 1);
+	read(fd, buffer, file_size);
+	buffer[file_size] = '\0';
+	return (buffer);
+}
+
+
+char **split_lines(char *buffer)
+{
+	uint num_lines = 0;
+	int i;
+	char **lines, *line;
+	
+	for (i = 0; buffer[i]; i++)
+		if (buffer[i] == '\n')
+			num_lines++;
+	
+	lines = malloc(NPTRS(num_lines + 1));
+	if (!lines)
+		return (NULL);
+		
+	line = strtok(buffer, "\n");
+
+	for (i = 0; i < num_lines; i++)
+	{
+		lines[i] = line;
+		line = strtok(NULL, "\n");
+	}
+	lines[i] = NULL;
+	return (lines);
+}
+
+char **get_tokens(char *line)
+{
+	char **tokens, *opcode, *arg;
+	
+	tokens = malloc(NPTRS(3));
+	if (!tokens)
+		return (NULL);
+	
+	tokens[0] = NULL;
+	tokens[1] = NULL;
+	tokens[2] = NULL;
+	
+	opcode = strtok(line, "\t ");
+	if (!opcode || !*opcode)
+		return (tokens);
+	if (strchr(opcode, ' '))
+	       *(strchr(opcode, ' ')) = '\0';
+	if (strchr(opcode, '\t'))
+               *(strchr(opcode, '\t')) = '\0';
+	tokens[0] = opcode;
+
+	arg = strtok(NULL, "\t ");
+        if (!arg || !*arg)
+                return (tokens);
+        if (strchr(arg, ' '))
+               *(strchr(arg, ' ')) = '\0';
+        if (strchr(arg, '\t'))
+               *(strchr(arg, '\t')) = '\0';
+	tokens[1] = arg;
+	
+	return (tokens);
+}
+
---
+++ monty.h
@@ -0,0 +1,95 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define NPTRS(size) (sizeof(void *) * (size))
+
+typedef unsigned int uint;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, uint line_number);
+} instruction_t;
+
+typedef struct state
+{
+	int stack_mode;
+	size_t length;
+	uint line_no;
+	void *buffer;
+	void *lines;
+	void *tokens;
+} state_t;
+
+char *read_file(int fd);
+char **split_lines(char *buffer);
+char **get_tokens(char *line);
+
+int print_node(stack_t *node, int ascii_val);
+int print_nodes(stack_t *node, int ascii_val);
+void free_stack(stack_t *node);
+
+
+void push(stack_t **stack, int value);
+void enqueue(stack_t **stack, int value);
+void stack_mode(void);
+void queue_mode(void);
+
+
+void pop(stack_t **stack, uint line_number);
+void pint(stack_t **stack, uint line_number);
+void pall(stack_t **stack, uint line_number);
+void pchar(stack_t **stack, uint line_number);
+void pstring(stack_t **stack, uint line_number);
+void swap(stack_t **stack, uint line_number);
+void nop(stack_t **stack, uint line_number);
+void add(stack_t **stack, uint line_number);
+void sub(stack_t **stack, uint line_number);
+void mul(stack_t **stack, uint line_number);
+void div_(stack_t **stack, uint line_number);
+void mod(stack_t **stack, uint line_number);
+void rotl(stack_t **stack, uint line_number);
+void rotr(stack_t **stack, uint line_number);
+
+
+
+extern state_t *state;
+extern stack_t *stack;
+
+
+
+
+
+#endif
